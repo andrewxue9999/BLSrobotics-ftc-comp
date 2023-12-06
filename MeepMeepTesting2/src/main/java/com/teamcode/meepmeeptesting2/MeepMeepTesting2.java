74d51accafa4b63a6046cbc2a0704debd97b66c3
AndrewXue07 <137796590+AndrewXue07@users.noreply.github.com>
2023-12-05 19:59:17 -0500

    added better auto path; added position variables
    still need to improve in the future

--- MeepMeepTesting2/src/main/java/com/teamcode/meepmeeptesting2/MeepMeepTesting2.java
+++ MeepMeepTesting2/src/main/java/com/teamcode/meepmeeptesting2/MeepMeepTesting2.java
@@ -11,6 +11,16 @@ public class MeepMeepTesting2 {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+        Pose2d blueStart1 = new Pose2d(-34, 70, Math.toRadians(-90));
+        Pose2d blueStart2 = new Pose2d(34, 70, Math.toRadians(-90));
+        Pose2d redStart1 = new Pose2d(-34, -70, Math.toRadians(90));
+        Pose2d redStart2 = new Pose2d(34, -70, Math.toRadians(90));
+
+        Vector2d bluePixel1 = new Vector2d(-35, 47);
+        Vector2d bluePixel2 = new Vector2d(11, 47);
+        Vector2d redPixel1 = new Vector2d(-35, -47);
+        Vector2d redPixel2 = new Vector2d(11, -47);
+
         // beginning position
         Pose2d startPose = new Pose2d(36, 70, Math.toRadians(-90));
 
@@ -21,11 +31,19 @@ public class MeepMeepTesting2 {
 
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(startPose)
-                                .splineTo(new Vector2d(11, 38), Math.toRadians(90)) // PROGRAM PROCESS TO DETECT PIXEL LOCATION
-                                .lineToLinearHeading(new Pose2d(11, 47, Math.toRadians(0))) // done detecting custom game piece and placing purple pixel
-                                .splineTo(new Vector2d(51, 33), Math.toRadians(0)) // place yellow pixel based on custom game piece position
-                                .splineToConstantHeading(new Vector2d(-51, 33), Math.toRadians(0)) // "back" is inaccurate because it relies on relative position, and regular splining takes too long
+                                .splineTo(new Vector2d(11, 47), Math.toRadians(-90)) // PROGRAM PROCESS TO DETECT PIXEL LOCATION
+                                .waitSeconds(2)
+                                .strafeTo(new Vector2d(11, 29)) // place purple pixel
+                                .waitSeconds(2)
+                                .strafeTo(new Vector2d(11, 47))
+                                .lineToSplineHeading(new Pose2d(50, 35, Math.toRadians(0))) // place yellow pixel
+                                .waitSeconds(2)
+                                .strafeTo(new Vector2d(-10, 61)) // "back" is inaccurate because it relies on relative position, and regular splining takes too long
+                                .strafeTo(new Vector2d(-51, 33))
+                                .waitSeconds(1)
+                                .splineToConstantHeading(new Vector2d(-10, 61), Math.toRadians(0))
                                 .splineToConstantHeading(new Vector2d(51, 33), Math.toRadians(0)) // place white pixels; end
+                                .waitSeconds(2)
                                 .build()
                 );
 
