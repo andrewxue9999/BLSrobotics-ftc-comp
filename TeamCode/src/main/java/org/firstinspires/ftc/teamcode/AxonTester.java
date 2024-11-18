325270900111ad96e28e6c9a6d85f30fc3a1e539
yarilightcav <71334766+badgamer2007@users.noreply.github.com>
2024-11-17 21:17:35 -0500

    added trying to get these poopyvoltages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AxonTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AxonTester.java
@@ -13,12 +13,21 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class AxonTester extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
-    private AnalogInput axon = null;
+    private AnalogInput axon1 = null;
+    private AnalogInput axon2;
+    private AnalogInput axon3;
+    private AnalogInput axon4;
 
     @Override
     public void runOpMode() {
 
-        axon = hardwareMap.analogInput.get("efrontRight");
+        axon1 = hardwareMap.analogInput.get("efrontRight");
+        axon2 = hardwareMap.analogInput.get("efrontLeft");
+        axon3 = hardwareMap.analogInput.get("ebackLeft");
+        axon4 = hardwareMap.analogInput.get("ebackRight");
+
+
+
 
         waitForStart();
         runtime.reset();
@@ -29,7 +38,7 @@ public class AxonTester extends LinearOpMode {
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("front right voltage", "%4.2f", axon.getVoltage());
+            telemetry.addData("svoltage", "%4.2f, %4.2f, %4.2f %4.2f", axon1.getVoltage(), axon2.getVoltage(), axon3.getVoltage(), axon4.getVoltage());
             telemetry.update();
         }
     }}
