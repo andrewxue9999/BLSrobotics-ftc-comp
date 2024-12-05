0d90b4a120ea659bd474df5c2acab3f056e8117a
David Wang <73254509+ducc-coder@users.noreply.github.com>
2024-12-05 15:31:02 -0500

    Update DifferentialWrist.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/hardware/DifferentialWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/hardware/DifferentialWrist.java
@@ -5,4 +5,10 @@
 package org.firstinspires.ftc.teamcode.robot.hardware;
 
 public class DifferentialWrist {
+    public void init(@NonNull HardwareMap hardwareMap, String name) {
+        wrist = hardwareMap.get(Servo.class, name);
+    }
+    public String getTelemetry(String name) {
+        return String.format("Open %S", name);
+    }
 }
