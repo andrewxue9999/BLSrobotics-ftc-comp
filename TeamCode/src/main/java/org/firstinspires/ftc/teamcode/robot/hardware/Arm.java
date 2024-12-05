1ce4e5ea128186b9b541df9bb4b3f891d5c9263b
David Wang <73254509+ducc-coder@users.noreply.github.com>
2024-12-05 15:38:09 -0500

    Update Arm.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/hardware/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/hardware/Arm.java
@@ -3,8 +3,9 @@
 package org.firstinspires.ftc.teamcode.robot.hardware;
 
 public class Arm {
+    private DcMotor arm;
     public void init(@NonNull HardwareMap hardwareMap, String name) {
-        arm = hardwareMap.get(Servo.class, name);
+        arm = hardwareMap.get(DcMotor.class, name);
     }
     public String getTelemetry(String name) {
         return String.format("Open %S", name);
